7a236a15c6de9e08bee6d2338e8b8795f572c4dd
Gilang Wirapati <wirapatigilang@gmail.com>
2026-07-16 18:32:43 +0700

    fix: update database connection string from DATABASE_URL to DIRECT_URL

--- web-desa-pranggong/src/lib/prisma.ts
+++ web-desa-pranggong/src/lib/prisma.ts
@@ -5,7 +5,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg({ connectionString: process.env.DIRECT_URL });
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
